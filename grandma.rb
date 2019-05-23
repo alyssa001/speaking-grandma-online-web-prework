ff9d67a04615035f4400e16cc02cfcecd735cef1
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-23 22:11:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
-def speak_to_grandma(say)
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+enddef speak_to_grandma(say)
   
   if say != say.upcase
     return "HUH?! SPEAK UP SONNY!"
